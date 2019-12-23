6c441fa753a69026016168432d31c23d8a91c544
Daquan Fletcher <dainaimic>
2019-12-23 17:12:13 -0500

    completed

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -5,25 +5,25 @@
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
-function CuboidMaker(length, width, height) {
-  this.length = length;
-  this.width = width;
-  this.height = height;
+function CuboidMaker(attributes) {
+  this.length = attributes.length;
+  this.width = attributes.width;
+  this.height = attributes.height;
 }
 
 /* == Step 2: Volume Method ==
   Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
   Formula for cuboid volume: length * width * height
 */
-CuboidMaker.prototype.volume = function () {
- return this.length * this.width * this.height;
+CuboidMaker.prototype.volume = function (volume) {
+  return this.length * this.width * this.height;
 } 
 
 /* == Step 3: Surface Area Method ==
   Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
-CuboidMaker.prototype.surfaceArea = function() {
+CuboidMaker.prototype.surfaceArea = function(surfaceArea) {
   return 2 * (this.length * this.width + this.length * this.height + this.width * this.height) 
 }
 
@@ -31,10 +31,12 @@ CuboidMaker.prototype.surfaceArea = function() {
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
-const cuboid = new CuboidMaker();
-cuboid.length = 4;
-cuboid.width = 5;
-cuboid.height = 5;
+const cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5
+})
+
   
 
 
